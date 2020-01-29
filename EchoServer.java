07fe0479a0711169bae3ec9acc379612f09e8b07
Doaa-Ziadat <doaaziadat@gmail.com>
2020-01-29 02:25:13 +0200

    Adding Part1

--- EchoServer.java
+++ EchoServer.java
@@ -258,31 +258,6 @@ public class EchoServer extends AbstractServer
 			this.handleMessageFromServerUI("error viewing item");
 			break;
 		}
-<<<<<<< HEAD:EchoServer.java
-		
-		
-	case "Discount":
-		Item item5=new Item();
-		try {
-			String s;
-			boolean f=item5.discount(Integer.parseInt(detail[1]),Double.parseDouble(detail[2]));
-			if(f) {  s="item discounted";
-			System.out.println(s);
-			}
-			else 
-				s="error";
-				
-			this.handleMessageFromServerUI(s);
-
-		} catch (NumberFormatException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			this.handleMessageFromServerUI("error viewing item");
-			break;
-		}
-	
-		}
-=======
 		
 		
 	case "Discount":
@@ -376,7 +351,6 @@ public class EchoServer extends AbstractServer
 			}
 	
 	
->>>>>>> 3d757023551d3eaa669b92f58a92fb6bc00f6dc5:src/main/java/EchoServer.java
 		
 		case "ViewOrder":
 		Order Or=new Order();
@@ -398,7 +372,6 @@ public class EchoServer extends AbstractServer
 			break;
 		}
 		
-
 		
 		case "ViewAllOrders":
 			String mseg="";
